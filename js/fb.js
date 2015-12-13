16a1a3befef4386d5cd3954476d9952e0a7fced2
Spellchaser <KangqiaoLei@gmail.com>
2015-12-13 04:50:14 -0500

    Additional Options working

--- js/fb.js
+++ js/fb.js
@@ -7,7 +7,6 @@ var snapshot;
 
 var newData = myDataRef.push({name: name, text: text});
 var postID = newData.key();
-
 myDataRef.on('child_added', function(snapshot) {
   var message = snapshot.val();
   console.log(snapshot);
@@ -20,17 +19,50 @@ function onLoginClick() {
 }
 
 var db = new Firebase('https://cenemus.firebaseio.com/')
-var opti = $('#polldata .opt').length;
-
+// Allows for empty polls. Possible for users to enter values. 
+// TODO: Think how to remove exploit. Bots could easily spam or "save poll names"
 function getPollOp() {
-	docPollOps = $('input[name^=opt]');
-	toSendPollOps = new Array($('#polldata .opt').length);
-	for (i = 0; i < $('polldata .opt').length; i++) {
-		toSendPollOps[i] = docPollOps[i].placeholder;
+	var opts = $('input[name^=opt]');//new Array($('#polldata .opt').length);
+	var rops = [];
+	for (var i = 0; i < $('#polldata .opt').length; i++) {
+		if(opts[i].value) {
+			rops.push(opts[i].value);
+		}
 	}
-	return toSendPollOps;
+	return rops;
 }
 
+function remOp(){
+	$('#last_opt').remove();
+}
+
+function addOp(){
+	$('#extra_opt').removeAttr("oninput").removeAttr("id").attr("placeholder","Enter an option here.");
+	$('#extra_optl').removeAttr("id");
+	/*
+	.on('change keyup copy paste cut', function() {
+        if (!this.value && this.is(":focus")) {remOp();}
+	});
+	function remOp(){
+		$('#last_opt').remove();
+	}
+	*/
+	var opti = $('#polldata .opt').length;
+	$('#optl').append(
+		"		<li><input type=\"text\" name=\"opt\" class=\"opt\" id=\"extra_opt\" oninput=\"addOp(this)\" value=\"\" placeholder=\"Enter an extra option!\"></li>"
+	);
+}
+
+function submitPoll(){
+	//$("#polldata").attr("action", "/" + newPollID).submit();
+	window.location = "https://www.google.com"; 
+	return false;
+}
+
+function createPoll(){
+	window.location = "https://www.youtube.com"; 
+	return false;
+}
 
 /*
 https://www.firebase.com/docs/web/guide/saving-data.html
@@ -39,6 +71,8 @@ https://www.firebase.com/docs/web/recipes.html
 https://cenemus.firebaseio.com/?page=Auth
 https://player.vimeo.com/video/55640600
 https://www.firebase.com/docs/security/api/rule/
+http://stackoverflow.com/questions/14963776/get-users-by-name-property-using-firebase
+http://stackoverflow.com/questions/25859514/how-to-get-current-user-on-firebase
 
 {
   "Op" : [ null, {
