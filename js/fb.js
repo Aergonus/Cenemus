b3c9c640e0fceff0f3c94c5d8f513a36e8175586
Spellchaser <Spellchaser@users.noreply.github.com>
2015-12-31 23:23:09 -0500

    Update fb.js
    Small bug when not loading poll

--- js/fb.js
+++ js/fb.js
@@ -79,50 +79,53 @@ var hostnameRegexp = new RegExp('^https?://.+?/');
 var pollKey;
 var pollInfo;
 function oninit() {
-	console.log("INIT!");
-	myDataRef.child("Lookup").child(location.hash.replace("#","")).once('value', function(snap) {
-		pollKey = snap.val();
-		
-	if (pollKey) {
-		myDataRef.child("PollDB").child(pollKey).once('value', function(snap) {
-			pollInfo = snap.val();
+	if (location.hash.replace("#","")) {
+		myDataRef.child("Lookup").child(location.hash.replace("#","")).once('value', function(snap) {
+			pollKey = snap.val();
+			
+		if (pollKey) {
+			myDataRef.child("PollDB").child(pollKey).once('value', function(snap) {
+				pollInfo = snap.val();
+			});
+			$('#mirror-wrapper').hide();
+			$('#polldata').hide();
+			// TODO : LOAD MAP WITH THE LOCATIONS
+			$('#map-wrapper').hide();
+
+			$('<h3 type="text" name="que" value="">'+pollInfo.Que+'</h3>').appendTo($('#pollInfo-wrapper'));
+			for (op in pollInfo.Op) {
+				$('<label><input type="checkbox" class="radio" pid="'+pollInfo.Op[op].PlaceId+'" value="'+op+'" name="poll" />'+pollInfo.Op[op].Value+'</label><br>').appendTo($('#pollInfo-wrapper'));
+			}
+			$('<button type="button" id="vote" onClick="submitVote()" value="Submit Vote!">Submit Vote!</button>').appendTo($('#pollInfo-wrapper'));
+			
+		// the selector will match all input controls of type :checkbox
+		// and attach a click event handler 
+		$("input:checkbox").on('click', function() {
+		  // in the handler, 'this' refers to the box clicked on
+		  var $box = $(this);
+		  if ($box.is(":checked")) {
+			// the name of the box is retrieved using the .attr() method
+			// as it is assumed and expected to be immutable
+			var group = "input:checkbox[name='" + $box.attr("name") + "']";
+			// the checked state of the group/box on the other hand will change
+			// and the current value is retrieved using .prop() method
+			$(group).prop("checked", false);
+			$box.prop("checked", true);
+		  } else {
+			$box.prop("checked", false);
+		  }
 		});
-		$('#mirror-wrapper').hide();
-		$('#polldata').hide();
-		// TODO : LOAD MAP WITH THE LOCATIONS
-		$('#map-wrapper').hide();
 
-		$('<h3 type="text" name="que" value="">'+pollInfo.Que+'</h3>').appendTo($('#pollInfo-wrapper'));
-		for (op in pollInfo.Op) {
-			$('<label><input type="checkbox" class="radio" pid="'+pollInfo.Op[op].PlaceId+'" value="'+op+'" name="poll" />'+pollInfo.Op[op].Value+'</label><br>').appendTo($('#pollInfo-wrapper'));
+
+		} else {
+			$('#pollInfo-wrapper').hide();
+			initMap();
 		}
-		$('<button type="button" id="vote" onClick="submitVote()" value="Submit Vote!">Submit Vote!</button>').appendTo($('#pollInfo-wrapper'));
-		
-	// the selector will match all input controls of type :checkbox
-	// and attach a click event handler 
-	$("input:checkbox").on('click', function() {
-	  // in the handler, 'this' refers to the box clicked on
-	  var $box = $(this);
-	  if ($box.is(":checked")) {
-		// the name of the box is retrieved using the .attr() method
-		// as it is assumed and expected to be immutable
-		var group = "input:checkbox[name='" + $box.attr("name") + "']";
-		// the checked state of the group/box on the other hand will change
-		// and the current value is retrieved using .prop() method
-		$(group).prop("checked", false);
-		$box.prop("checked", true);
-	  } else {
-		$box.prop("checked", false);
-	  }
-	});
-
-
+		});
 	} else {
 		$('#pollInfo-wrapper').hide();
 		initMap();
 	}
-	});
-
 }
 
 
